c9275cbf2fb06101a36e3b6c14da71d0b276550f
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2024-07-06 19:22:00 -0700

    Added WKHTML_PDF_BINARY and WKHTML_IMG_BINARY environment variables, and modified an invoice table migration to wrap changes in a try-catch block.

--- database/migrations/2024_04_17_074946_alter_invoices_table_change_account_id_and_ref_columns.php
+++ database/migrations/2024_04_17_074946_alter_invoices_table_change_account_id_and_ref_columns.php
@@ -4,16 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('invoices', function (Blueprint $table) {
-            $table->unsignedInteger("account_id")->nullable()->after("client_id")->change();
-            $table->string("ref")->nullable()->after("account_id")->change();
-        });
+        try {
+            Schema::table('invoices', function (Blueprint $table) {
+                $table->unsignedInteger("account_id")->nullable()->after("client_id")->change();
+                $table->string("ref")->nullable()->after("account_id")->change();
+            });
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 
     /**
@@ -22,7 +27,6 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('invoices', function (Blueprint $table) {
-
         });
     }
 };
